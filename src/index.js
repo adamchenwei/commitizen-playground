48527ed0f03b525ed6e762e8817a197ad516c04e
Adam Chen Wei <awei@pebblepost.com>
2017-11-28 10:13:22 -0500

    docs(just beebee):

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 function abc() {
   console.log('no no no its not a test!!!!');
+  console.log('beebee')
 }
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
